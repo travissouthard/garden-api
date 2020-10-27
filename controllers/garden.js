1152dbef56c93c98e3d164c01a37563af6a89cbe
Travis Southard <tsouthard88@gmail.com>
2020-10-27 16:08:59 -0400

    Ran npm init, because apparently I forgot that whole step

--- controllers/garden.js
+++ controllers/garden.js
@@ -2,9 +2,17 @@
 const express = require("express")
 const garden = express.Router()
 
+const Garden = require("../models/garden.js")
+
 //Routes
 garden.get("/", (req, res) => {
-    res.send("Oh hell yeah! Garden Time!")
+    res.send("<h1>Travis' Garden</h1>")
+    // Garden.find({}, (err, foundGardens) => {
+    //     if (err) {
+    //         res.status(400).json({"Error": err.message})
+    //     }
+    //     res.status(200).json(foundGardens)
+    // })
 })
 
 module.exports = garden
